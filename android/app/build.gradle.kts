497cdbc668786b88abe7ba0b2db93eddd925659f
bilgeedev <jrrsenpai@gmail.com>
2026-05-22 16:37:03 +0800

    z

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -27,7 +27,7 @@ android {
     packaging {
         jniLibs {
             // PAX DAL (DeviceConfig) expects extracted .so files under app nativeLibraryDir.
-            useLegacyPackaging = true
+            useLegacyPackaging = false
         }
     }
 
